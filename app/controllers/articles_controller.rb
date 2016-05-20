473595038a03725cc969033051a1b19d7aa872d5
kashif nawaz <danishn786@gmail.com>
2016-05-20 20:58:06 +0530

    added new and show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,17 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles = Article.all
+
+	end
+
   def new
   	@article = Article.new
 
+  end
+
+  def edit
+  	@article = Article.find(params[:id])
   end
 
   def create
@@ -15,6 +24,16 @@ class ArticlesController < ApplicationController
     	render 'new'
     end
 
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	if @article.update(article_params)
+  		flash[:notice] = "Article was successfuly updated"
+  		redirect_to article_path(@article)
+  	else
+  		render 'edit'
+  	end
   end
 
   def show
