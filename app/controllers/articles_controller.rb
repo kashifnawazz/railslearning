1ad3604bbde805de4f21e6b6e8b12e050de60a03
kashif nawaz <danishn786@gmail.com>
2016-05-31 20:44:07 +0530

    correction

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
   def create
     
   	@article = Article.new(article_params)
-    @article.user = cuurent_first
+    @article.user = current_user
     if @article.save
     	flash[:success] = "Article was successfuly created"
     	redirect_to article_path(@article)
